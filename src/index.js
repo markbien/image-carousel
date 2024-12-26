7cb589f673baa0a5e6de94fa52895a6e1c7b909b
Mark Bien <48498353+markbien@users.noreply.github.com>
2024-12-26 15:58:15 +0800

    Add functions

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
-const cards = document.querySelectorAll('.card');
+const cards = document.querySelectorAll(".card");
+let currentIndex = 0;
 
-console.log(cards);
+function nextSlide() {
+  const currentCard = cards[currentIndex];
+  currentCard.classList.remove("active");
+
+  currentIndex++;
+  if (currentIndex == cards.length) {
+    currentIndex = 0;
+  }
+
+  cards[currentIndex].classList.add("active");
+}
+
+function prevSlide(){
+  const currentCard = cards[currentIndex];
+  currentCard.classList.remove("active");
+
+  currentIndex--;
+  if (currentIndex < 0) {
+    currentIndex = cards.length - 1;
+  }
+
+  cards[currentIndex].classList.add("active");
+}
+
+const rightArrow = document.querySelector(".right-arrow");
+rightArrow.addEventListener("click", nextSlide);
+
+const leftArrow = document.querySelector(".left-arrow");
+leftArrow.addEventListener("click", prevSlide);
