fa96ffb5363acaff13ed673e1415aac6243c160c
Mark Bien <48498353+markbien@users.noreply.github.com>
2024-12-28 04:32:26 +0800

    Add dot images, and function to select current dot

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 const cards = document.querySelectorAll(".card");
+const dots = document.querySelectorAll('.navigation-dots img');
 let currentIndex = 0;
 
 function nextSlide() {
   const currentCard = cards[currentIndex];
   currentCard.classList.remove("active");
+
+  const currentDot = dots[currentIndex];
+  currentDot.setAttribute('src', '/src/images/dot.png');
 
   currentIndex++;
   if (currentIndex == cards.length) {
@@ -11,11 +15,15 @@ function nextSlide() {
   }
 
   cards[currentIndex].classList.add("active");
+  dots[currentIndex].setAttribute('src', '/src/images/dot-active.png');
 }
 
 function prevSlide(){
   const currentCard = cards[currentIndex];
   currentCard.classList.remove("active");
+
+  const currentDot = dots[currentIndex];
+  currentDot.setAttribute('src', '/src/images/dot.png');
 
   currentIndex--;
   if (currentIndex < 0) {
@@ -23,6 +31,7 @@ function prevSlide(){
   }
 
   cards[currentIndex].classList.add("active");
+  dots[currentIndex].setAttribute('src', '/src/images/dot-active.png');
 }
 
 const rightArrow = document.querySelector(".right-arrow");
